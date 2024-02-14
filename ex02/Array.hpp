35881a8618fc3806767e4c8947d5c391231a7848
AlanADX-dev <alan.andrieux@gmail.com>
2024-02-14 15:11:42 +0100

    correction with std98 norms

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -3,30 +3,26 @@
 
 #include <iostream>
 #include <cstddef>
+#include <cstdlib>
 
 template<typename T>
 class Array {
 
 	private:
 		T*		elements;
-		int		size;
+		int		m_size;
 	protected:
 
 	public:
-		/*Construction with no parameter: Creates an empty array*/
-		Array() : elements(nullptr), size(0) {}
+		Array() : elements(NULL), m_size(0) {}
 
-		/*Construction with an unsigned int n as a parameter: Creates an array of n elements
-		initialized by default.*/
-		Array(int n) : elements(new T[n]), size(n) {
-			for(int i = 0; i < size; ++i)
+		Array(int n) : elements(new T[n]), m_size(n) {
+			for(int i = 0; i < m_size; ++i)
 				elements[i] = T();
 		}
 
-		/*Construction by copy and assignment operator. In both cases, modifying either the
-		original array or its copy after copying musn’t affect the other array.*/
-		Array(const Array& other) : elements(new T[other.size]), size(other.size) {
-			for (int i = 0; i < size; ++i)
+		Array(const Array& other) : elements(new T[other.m_size]), m_size(other.m_size) {
+			for (int i = 0; i < m_size; ++i)
 				elements[i] = other.elements[i];
 		}
 
@@ -38,27 +34,23 @@ class Array {
 			if (this != &other)
 			{
 				delete[] elements;
-				elements = new T[other.size];
-				size = other.size;
-				for (int i = 0; i < size; ++i)
+				elements = new T[other.m_size];
+				m_size = other.m_size;
+				for (int i = 0; i < m_size; ++i)
 					elements[i] = other.elements[i];
 			}
 			return *this;
 		}
 
-		/*When accessing an element with the [ ] operator, if its index is out of bounds, an
-		std::exception is thrown.*/
 		T& operator[](int index) {
-			if (index >= size) {
+			if (index >= m_size) {
 				throw std::out_of_range("Index out of bounds");
 			}
 			return elements[index];
 		}
 
-		/*A member function size() that returns the number of elements in the array. This
-		member function takes no parameter and musn’t modify the current instance*/
 		int		size() {
-			return size;
+			return m_size;
 		}
 };
 
